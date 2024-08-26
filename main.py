add0196a174d709f5a154cb4dbdc3bab2f6936dc
KauaOrtiz <kauaortizz@gmail.com>
2024-08-25 21:56:51 -0300

    Feat: implement a login interface to employee and enterprise

--- main.py
+++ main.py
@@ -1,20 +1,59 @@
-from flask import Flask, json, render_template, request
+from flask import Flask, json, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-with open("alunos.json", "r") as f:
-    alunos = json.load(f)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/login/employee")
+@app.route("/login/employee", methods=['GET', 'POST'])
 def loginEmployee():
-    return render_template("login_employee.html")
+    if request.method == 'POST':
+        if 'username' in request.form:
+            username = request.form['username']
+            password = request.form['password']
+            if username == 'admin' and password == 'password':
+                return redirect(url_for('dashboardEmployee'))
+            else:
+                error = 'Invalid Credentials'
+                return render_template('login_employee.html', error=error)
+        elif 'new-username' in request.form:
+            new_username = request.form['new-username']
+            enterprise = request.form['enterprise']
+            position = request.form['position']
+            new_password = request.form['new-password']
+            confirm_password = request.form['confirm-password']
+            if new_password == confirm_password:
+                sucess = "User create! Login now!"
+                return render_template('login_employee.html', sucess=sucess)
+            else:
+                error = "Passwords don't match"
+                return render_template('login_employee.html', error=error)
 
-@app.route("/login/enterprise")
+    return render_template('login_employee.html')
+
+@app.route("/login/enterprise", methods=['GET', 'POST'])
 def loginEnterprise():
-    return render_template("login_enterprise.html")
+    if request.method == 'POST':
+        if 'company' in request.form:
+            company = request.form['company']
+            password = request.form['password']
+            if company == 'admin' and password == 'password':
+                return redirect(url_for('dashboardEnterprise'))
+            else:
+                error = 'Invalid Credentials'
+                return render_template('login_enterprise.html', error=error)
+        elif 'new-company' in request.form:
+            new_company = request.form['new-company']
+            new_password = request.form['new-password']
+            confirm_password = request.form['confirm-password']
+            if new_password == confirm_password:
+                sucess = "Enterprise create! Login now!"
+                return render_template('login_enterprise.html', sucess=sucess)
+            else:
+                error = "Passwords don't match"
+                return render_template('login_enterprise.html', error=error)
+    return render_template('login_enterprise.html')
 
 @app.route("/dashboard/employee")
 def dashboardEmployee():
